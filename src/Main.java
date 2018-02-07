c0eb531fa33278cb30deb4c129a37fd82b76a3ba
MariusDK <mariusdk7@gmail.com>
2018-02-07 17:20:01 +0200

    Shanks var

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
-import encryption.Encryption;
-import encryption.KeyGenerator;
-import keys.PrivateKey;
-import keys.PublicKey;
+import decrypt.Shanks;
 import utils.CyclicGroup;
-import utils.Pow;
 import utils.PrimeNumber;
 import utils.SafePrimeNumber;
 
@@ -14,26 +10,14 @@ public class Main {
     public static void main(String[] args) {
 //        CyclicGroup cg = new CyclicGroup(4);
 //        System.out.println(cg.getRandomElementFromGroup() + " " + "\n");
-//        System.out.println(cg.getCyclicGroupGenerator(new SafePrimeNumber(5,2)));
+//        System.out.println(cg.getCyclicGroupGenerator());
 //        PrimeNumber pn = new PrimeNumber();
-//        SafePrimeNumber p = pn.getSafeRandomPrimeNumber();
-//        System.out.println(p.getP() + " " + pn.isPrimeLong(p.getP()));
-//        System.out.println(p.getP() / 2 + " " + pn.isPrimeLong(p.getP()));
-//        System.out.println(p.getP() % 2);
-//        KeyGenerator kg = new KeyGenerator();
-//        PublicKey publicKey = kg.generatePublicKey();
-//        System.out.println(publicKey.getG() + " " + publicKey.getY() + " " + publicKey.getP());
-//        System.out.println("G:" + publicKey.getG() + " " + "Y:" + publicKey.getY() + " " + "P:" + publicKey.getP());
-//        PrivateKey privateKey = kg.generatePrivateKey();
-//        System.out.println("X:" + privateKey.getX());
-//        String s = "Mesaj";
-        Encryption e = new Encryption();
-        PublicKey p = e.encrypt("Mesaj de criptat");
-        System.out.println(p.getG() + " " + p.getP() + " " + p.getY());
-//        Shanks s = new Shanks();
-//        s.ShanksAlgoritm(3, 525, 809);
-//        s.printList1();
-        //System.out.println(Pow.pow(9,10));
+//        SafePrimeNumber spn = pn.getSafeRandomPrimeNumber();
+//        System.out.println(spn.getP() + " " + spn.getQ());
+        Shanks shanks = new Shanks();
+        System.out.println(shanks.ShanksAlgoritm(3,525,809));
+        shanks.printList2();
+
     }
 
 }
