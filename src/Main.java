1106cf3c42eba5371542dfa8dccb454d64e5e4cc
cosofretg <cosofret.geo@gmail.com>
2018-02-08 00:40:37 +0200

    SCR: Override pow method and change parameters for generating prime numbers

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import encryption.KeyGenerator;
 import keys.PrivateKey;
 import keys.PublicKey;
 import utils.CyclicGroup;
+import utils.Pow;
 import utils.PrimeNumber;
 import utils.SafePrimeNumber;
 
@@ -13,7 +14,7 @@ public class Main {
     public static void main(String[] args) {
 //        CyclicGroup cg = new CyclicGroup(4);
 //        System.out.println(cg.getRandomElementFromGroup() + " " + "\n");
-//        System.out.println(cg.getCyclicGroupGenerator());
+//        System.out.println(cg.getCyclicGroupGenerator(new SafePrimeNumber(5,2)));
 //        PrimeNumber pn = new PrimeNumber();
 //        SafePrimeNumber p = pn.getSafeRandomPrimeNumber();
 //        System.out.println(p.getP() + " " + pn.isPrimeLong(p.getP()));
@@ -21,13 +22,18 @@ public class Main {
 //        System.out.println(p.getP() % 2);
 //        KeyGenerator kg = new KeyGenerator();
 //        PublicKey publicKey = kg.generatePublicKey();
+//        System.out.println(publicKey.getG() + " " + publicKey.getY() + " " + publicKey.getP());
 //        System.out.println("G:" + publicKey.getG() + " " + "Y:" + publicKey.getY() + " " + "P:" + publicKey.getP());
 //        PrivateKey privateKey = kg.generatePrivateKey();
 //        System.out.println("X:" + privateKey.getX());
-        String s = "Mesaj";
+//        String s = "Mesaj";
         Encryption e = new Encryption();
-        PublicKey p = e.encrypt(s);
+        PublicKey p = e.encrypt("Mesaj de criptat");
         System.out.println(p.getG() + " " + p.getP() + " " + p.getY());
+//        Shanks s = new Shanks();
+//        s.ShanksAlgoritm(3, 525, 809);
+//        s.printList1();
+        //System.out.println(Pow.pow(9,10));
     }
 
 }
